a5b41cab7c93bf08ad4b52f658d9fb0ba460ae6b
Ion D. Filho <iondrimba@gmail.com>
2017-10-01 20:16:37 -0300

    secode demo

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ function App() {
   var audioCtx = new (window.AudioContext || window.webkitAudioContext)();
   var analyser = audioCtx.createAnalyser();
 
-  var file = 'demo.mp3';
+  var file = 'second-demo.mp3';
   var playing = false;
   var canvasWidth = document.body.clientWidth;
   audioElement.volume = .1;
@@ -31,7 +31,7 @@ function App() {
     playing = false;
   });
   audioElement.addEventListener('ended', function () {
-    playing = true;
+    playing = false;
   });
 
   window.onresize = function () {
@@ -39,7 +39,7 @@ function App() {
     drawCanvasWidth();
   };
 
-  this.loadSound = function() {
+  this.loadSound = function () {
     var request = new XMLHttpRequest();
     request.open('GET', file, true);
     request.onprogress = function (evt) {
@@ -70,7 +70,10 @@ function App() {
 
   function drawBar(left, top, width, height, color) {
     ctx.fillStyle = color;
-    ctx.fillRect(left, top, width, height);
+    ctx.beginPath();
+    ctx.arc(left, height, 5, 0,2*Math.PI);
+    ctx.fillStyle = color;
+    ctx.fill();
   }
 
   function drawWave() {
@@ -80,30 +83,33 @@ function App() {
       ctx.clearRect(0, 0, canvas.width, canvas.height);
       var width = 5;
       for (var j = 0; j < canvas.width; j++) {
-        var left = (width + 1) * j;
+        var left = (width +10 ) * j;
         var topByte = (frequencyData[j]);
 
         var y = (topByte);
 
         if ((y > 80) && (y < 200)) {
-          drawBar(left, y + y, width, +5, '#d2f3ff');
+          drawBar(left, y + y, width, y+25, '#d2f3ff');
         }
 
-        if ((y > 55) && (y < 80)) {
-          drawBar(left, y + y, width, +5, '#275cf4');
+        if ((y > 55) && (y <= 80)) {
+          drawBar(left, y + y, width, +80, '#275cf4');
         }
 
-        if ((y > 50) && (y < 55)) {
-          drawBar(left, y + y, width, +5, '#4e75c2');
+        if ((y > 50) && (y <= 55)) {
+          drawBar(left, y + y, width, +60, '#4e75c2');
         }
 
-        if ((y > 45) && (y < 50)) {
-          drawBar(left, y + y, width, +5, '#edfef6');
+        if ((y > 45) && (y <= 50)) {
+          drawBar(left, y + y, width, +40, '#edfef6');
         }
 
-        drawBar(left, 0, width, y, '#cd0126');
 
-        drawBar(left, 0, width, y - 50, '#860018');
+        if ((y > 10) && (y <= 45)) {
+          drawBar(left, y + y, width, 20, '#275cf4');
+        }
+
+        drawBar(left, 0, width, y - 50, '#cd0126');
       }
     }
 
