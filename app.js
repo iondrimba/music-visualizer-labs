894a7098b2dfc422608a574add3a16817047fcca
Ion D. Filho <iondrimba@gmail.com>
2017-10-01 18:49:13 -0300

    load sound method

--- app.js
+++ app.js
@@ -1,25 +1,45 @@
-class App {
-  constructor() {
-    var d = [];
-    var audioElement = document.getElementById('audioElement');
-    var audioCtx = new (window.AudioContext || window.webkitAudioContext)();
-    var analyser = audioCtx.createAnalyser();
+function App() {
+  var d = [];
+  var audioElement = document.getElementById('audioElement');
+  var audioCtx = new (window.AudioContext || window.webkitAudioContext)();
+  var analyser = audioCtx.createAnalyser();
 
-    var file = 'demo.mp3';
-    var playing = false;
-    var canvasWidth = document.body.clientWidth;
-    audioElement.volume = .1;
+  var file = 'demo.mp3';
+  var playing = false;
+  var canvasWidth = document.body.clientWidth;
+  audioElement.volume = .1;
 
-    audioElement.addEventListener('playing', function () {
-      playing = true;
-    });
-    audioElement.addEventListener('pause', function () {
-      playing = false;
-    });
-    audioElement.addEventListener('ended', function () {
-      playing = true;
-    });
+  var source = audioCtx.createMediaElementSource(audioElement);
+  source.connect(analyser);
+  source.connect(audioCtx.destination);
+  var bufferLength = analyser.frequencyBinCount;
+  var frequencyData = new Uint8Array(bufferLength);
+  var frequencyDataFloat = new Float32Array(bufferLength);
+  analyser.smoothingTimeConstant = .5;
 
+  var canvas = document.getElementById('wave');
+  drawCanvasWidth();
+  drawCanvasHeight();
+
+  var ctx = canvas.getContext('2d');
+  ctx.lineWidth = 1;
+
+  audioElement.addEventListener('playing', function () {
+    playing = true;
+  });
+  audioElement.addEventListener('pause', function () {
+    playing = false;
+  });
+  audioElement.addEventListener('ended', function () {
+    playing = true;
+  });
+
+  window.onresize = function () {
+    canvasWidth = document.body.clientWidth;
+    drawCanvasWidth();
+  };
+
+  this.loadSound = function() {
     var request = new XMLHttpRequest();
     request.open('GET', file, true);
     request.onprogress = function (evt) {
@@ -27,7 +47,7 @@ class App {
       var loading = document.getElementsByClassName('loading')[0];
       loading.innerHTML = 'loading... ' + percent + ' %';
 
-      if(percent==100) {
+      if (percent == 100) {
         loading.remove();
       }
     }
@@ -36,91 +56,66 @@ class App {
       audioElement.load();
     }
     request.send();
-
-    window.onresize = function () {
-      canvasWidth = document.body.clientWidth;
-      drawCanvasWidth();
-    };
-
-    function nearestPow2(aSize) {
-      return Math.pow(2, Math.round(Math.log(aSize) / Math.log(2)));
-    }
-
-    var source = audioCtx.createMediaElementSource(audioElement);
-    source.connect(analyser);
-    source.connect(audioCtx.destination);
-    var bufferLength = analyser.frequencyBinCount;
-    var frequencyData = new Uint8Array(bufferLength);
-    var frequencyDataFloat = new Float32Array(bufferLength);
-    analyser.smoothingTimeConstant = .5;
-
-    var canvas = document.getElementById('wave');
-    drawCanvasWidth();
-    drawCanvasHeight();
-
-    var ctx = canvas.getContext('2d');
-    ctx.lineWidth = 1;
-
-    function drawCanvasWidth() {
-      canvas.style.width = (document.getElementsByTagName('section')[0].getBoundingClientRect().width - 100) + 'px';
-      canvas.width = parseInt(canvas.style.width);
-    }
-
-    function drawCanvasHeight() {
-      canvas.style.height = '200px';
-      canvas.height = parseInt(canvas.style.height);
-    }
-
-    function drawBar(left, top, width, height, color) {
-      ctx.fillStyle = color;
-      ctx.fillRect(left, top, width, height);
-    }
-
-    function drawWave() {
-      if (playing) {
-        analyser.getByteFrequencyData(frequencyData)
-
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
-        var width = 5;
-        var h = 0;
-        for (var j = 0; j < canvas.width; j++) {
-          var w = width;
-          var left = (w + 1) * j;
-          var topByte = (frequencyData[j]);
-
-          var y = (topByte);
-
-          if ((y > 80) && (y < 200)) {
-            drawBar(left, y + y, width, +5, '#d2f3ff');
-          }
-
-          if ((y > 55) && (y < 80)) {
-            drawBar(left, y + y, width, +5, '#275cf4');
-          }
-
-          if ((y > 50) && (y < 55)) {
-            drawBar(left, y + y, width, +5, '#4e75c2');
-          }
-
-          if ((y > 45) && (y < 50)) {
-            drawBar(left, y + y, width, +5, '#edfef6');
-          }
-
-          drawBar(left, 0, width, y, '#cd0126');
-
-          drawBar(left, 0, width, y - 50, '#860018');
-        }
-      }
-
-      setTimeout(function () {
-        requestAnimationFrame(drawWave);
-      }, 30);
-    }
-
-    drawWave();
   }
+
+  function drawCanvasWidth() {
+    canvas.style.width = (document.getElementsByTagName('section')[0].getBoundingClientRect().width - 100) + 'px';
+    canvas.width = parseInt(canvas.style.width);
+  }
+
+  function drawCanvasHeight() {
+    canvas.style.height = '200px';
+    canvas.height = parseInt(canvas.style.height);
+  }
+
+  function drawBar(left, top, width, height, color) {
+    ctx.fillStyle = color;
+    ctx.fillRect(left, top, width, height);
+  }
+
+  function drawWave() {
+    if (playing) {
+      analyser.getByteFrequencyData(frequencyData)
+
+      ctx.clearRect(0, 0, canvas.width, canvas.height);
+      var width = 5;
+      for (var j = 0; j < canvas.width; j++) {
+        var left = (width + 1) * j;
+        var topByte = (frequencyData[j]);
+
+        var y = (topByte);
+
+        if ((y > 80) && (y < 200)) {
+          drawBar(left, y + y, width, +5, '#d2f3ff');
+        }
+
+        if ((y > 55) && (y < 80)) {
+          drawBar(left, y + y, width, +5, '#275cf4');
+        }
+
+        if ((y > 50) && (y < 55)) {
+          drawBar(left, y + y, width, +5, '#4e75c2');
+        }
+
+        if ((y > 45) && (y < 50)) {
+          drawBar(left, y + y, width, +5, '#edfef6');
+        }
+
+        drawBar(left, 0, width, y, '#cd0126');
+
+        drawBar(left, 0, width, y - 50, '#860018');
+      }
+    }
+
+    setTimeout(function () {
+      requestAnimationFrame(drawWave);
+    }, 30);
+  }
+
+  drawWave();
 }
 
 document.addEventListener('DOMContentLoaded', function () {
-  window.app = new App();
+  var app = new App();
+  app.loadSound();
 })
